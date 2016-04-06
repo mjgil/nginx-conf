323403a798ca1ab0fc5a1f09b0ae6394a0081a05
Malcom Gilbert <malcomgilbert@gmail.com>
2016-04-06 14:12:57 -0400

    update init

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # tutorial from https://www.digitalocean.com/community/tutorials/how-to-set-up-multiple-ssl-certificates-on-one-ip-with-nginx-on-ubuntu-12-04
-ln ~/git/nginxConf/malcomgilbert.com /etc/nginx/sites-available/malcomgilbert.com 
-ln ~/git/nginxConf/malcomgilbert.com /etc/nginx/sites-enabled/malcomgilbert.com 
+rm -rf /etc/nginx/sites-available/malcomgilbert.com 
+rm -rf /etc/nginx/sites-enabled/malcomgilbert.com 
+rm -rf /etc/nginx/sites-available/mjgil.com 
+rm -rf /etc/nginx/sites-enabled/mjgil.com 
 
-ln ~/git/nginxConf/mjgil.com /etc/nginx/sites-available/mjgil.com 
-ln ~/git/nginxConf/mjgil.com /etc/nginx/sites-enabled/mjgil.com 
+ln -s ~/git/nginxConf/malcomgilbert.com /etc/nginx/sites-available/malcomgilbert.com 
+ln -s ~/git/nginxConf/malcomgilbert.com /etc/nginx/sites-enabled/malcomgilbert.com 
+
+ln -s ~/git/nginxConf/mjgil.com /etc/nginx/sites-available/mjgil.com 
+ln -s ~/git/nginxConf/mjgil.com /etc/nginx/sites-enabled/mjgil.com 
